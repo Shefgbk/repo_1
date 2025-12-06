24a39212d611214bfb212342758b756a58b7a461
Shefgbk <shef.934@gmail.com>
2025-12-06 08:12:46 +0300

    final fixed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#from src.masks import get_mask_card_number, get_mask_account
-from src.widget import mask_account_card
+# from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_date
 
-#print(get_mask_card_number("7000792289606361"))
-#print(get_mask_account("73654108430135874305"))
-print(mask_account_card('Maestro 1596837868705199'))
-print(mask_account_card('Счет 64686473678894779589'))
-print(mask_account_card('Visa Classic 6831982476737658'))
+# print(get_mask_card_number("7000792289606361"))
+# print(get_mask_account("73654108430135874305"))
+print(mask_account_card("Maestro 1596837868705199"))
+print(mask_account_card("Счет 64686473678894779589"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(get_date("2024-03-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -5,8 +5,15 @@ def mask_account_card(bank_info: str) -> str:
     """Функция маскировки номеров карт и номеров счетов"""
     formated_string = bank_info.split()
     if formated_string[0] == "Счет":
-        return f'{formated_string[0]} {get_mask_account(formated_string[1])}'
+        return f"{formated_string[0]} {get_mask_account(formated_string[1])}"
     else:
         type_card = " ".join(formated_string[0:-1])
-        return f'{type_card} {get_mask_card_number(formated_string[-1])}'
+        return f"{type_card} {get_mask_card_number(formated_string[-1])}"
 
+
+def get_date(initial_date: str) -> str:
+    """Функция, изменяющая формат даты"""
+    list_of_date = initial_date.split("T")
+    formated_list = list_of_date[0].split("-")
+    formated_date = ".".join(formated_list[-1::-1])
+    return formated_date
